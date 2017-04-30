2014eab06e770250fc0b56c729e4505b259e7c0c
Wendy Reyes <wendy.reyessan@gmail.com>
2017-04-30 06:11:59 -0500

    subiendo estilos

--- assets/js/game0.js
+++ assets/js/game0.js
@@ -7,10 +7,18 @@ var completar3 = document.getElementById("input-3");
 var completar4 = document.getElementById("input-4");
 
 play1.addEventListener("click", function(event) {
-	if(completar1.value == "uno" && completar2.value == "dos" && completar3.value == "tres" && completar4.value == "cuatro") {
+
+	if(completar1.value == "habitable" && completar2.value == "Menos de 10%" && completar3.value == "90% del espacio" && completar4.value == "50 y un 80%") {
 		alert("Has ganado 4 puntos!!");
+		document.getElementById('completar').reset();
 		window.location = "game2.html";
 	} else {
-		alert("Incorrecto, vuelve a intentarlo");
+		if(completar1.value.trim().length == 0 || completar2.value.trim().length == 0 || completar3.value.trim().length == 0 || completar4.value.trim().length == 0) {
+			alert("Debes completar todos los campos");
+		} else {
+			document.getElementById('completar').reset();
+			alert("Incorrecto, vuelve a intentarlo");
+		}
 	}
+
 });
